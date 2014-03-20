2a3b34a21732ebb75d745b997af30d47bc260a49
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-20 20:48:04 +0700

    [1/2] Base: implement App circle sidebar
    add whitelist app to include into circle view
    rebased
    make standalone patch
    
    PS.15 : rebased
    PS.16 : cleanup
    
    Conflicts:
    	packages/SystemUI/res/values/cr_strings.xml
    
    Change-Id: I08d2a3a7e0f8df20e801e585d4cac8547a740a3e
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -907,6 +907,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateShowSearchHoldoff();
 
         addGestureAnywhereView();
+        addAppCircleSidebar();
 
         try {
             boolean showNav = mWindowManagerService.hasNavigationBar();
