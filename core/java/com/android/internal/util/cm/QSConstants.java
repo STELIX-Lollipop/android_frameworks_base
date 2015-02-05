2cdcb9d3eb253b3bd2b6e72b0ef5e047ded0a740
eyosen <abittin@gmail.com>
2015-02-05 13:49:09 +0200

    QS: add Screenrecord tile (1/2)
    Change-Id: I2df2ff0c8b81eb77b3d112d357cdf18cb19f4c2b

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -47,6 +47,7 @@ public class QSConstants {
     public static final String TILE_VISUALIZER = "visualizer";
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
     public static final String TILE_SCREENSHOT = "screenshot";
+    public static final String TILE_SCREENRECORD = "screenrecord";
     public static final String TILE_SYNC = "sync";
 
     // Order matters
@@ -85,6 +86,7 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_VISUALIZER);
         TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
         TILES_AVAILABLE.add(TILE_SCREENSHOT);
+        TILES_AVAILABLE.add(TILE_SCREENRECORD);
         TILES_AVAILABLE.add(TILE_SYNC);
     }
 }
