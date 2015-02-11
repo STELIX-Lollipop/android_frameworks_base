3df88e5e2b6c8da9ec3b7c107b9e839584343c14
STELIX <ssspinni@gmail.com>
2015-02-11 11:30:05 +0100

    Revert "Settings - Add SleepScreen QS Tile (PowerMenu on longpress) [2/2]"
    This reverts commit 2c98edbeaa05889f40d2770f1de9fd2bf01c341f.

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -48,7 +48,6 @@ public class QSConstants {
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
     public static final String TILE_SCREENSHOT = "screenshot";
     public static final String TILE_SCREENRECORD = "screenrecord";
-    public static final String TILE_SLEEPSCREEN = "sleepscreen";
     public static final String TILE_SYNC = "sync";
 
     // Order matters
@@ -88,7 +87,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
         TILES_AVAILABLE.add(TILE_SCREENSHOT);
         TILES_AVAILABLE.add(TILE_SCREENRECORD);
-        TILES_AVAILABLE.add(TILE_SLEEPSCREEN);
         TILES_AVAILABLE.add(TILE_SYNC);
     }
 }
