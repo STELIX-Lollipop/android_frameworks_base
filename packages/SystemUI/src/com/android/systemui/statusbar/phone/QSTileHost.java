1d8373281c4c92fccb9365846639d02676304de2
STELIX <ssspinni@gmail.com>
2015-02-11 11:27:10 +0100

    Revert "FW_Base - Add SleepScreen QS Tile (PowerMenu on longpress) [1/2] Fix"
    This reverts commit 695c1da7592aa78d6ac4cffdc4feef792aac9042.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -60,7 +60,6 @@ import com.android.systemui.qs.tiles.VisualizerTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
-import com.android.systemui.qs.tiles.SleepScreenTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
@@ -329,8 +328,6 @@ public class QSTileHost implements QSTile.Host {
                 return new ScreenshotTile(this);
             case QSConstants.TILE_SCREENRECORD:
                 return new ScreenrecordTile(this);
-            case QSConstants.TILE_SLEEPSCREEN:
-                return new SleepScreenTile(this);
             case QSConstants.TILE_SYNC:
                 return new SyncTile(this);
             default:
