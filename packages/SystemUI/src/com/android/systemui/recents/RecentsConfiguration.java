100b3f4bbacac0fd5af07f934b271b5fc04b838f
STELIX <ssspinni@gmail.com>
2015-03-12 10:55:27 +0100

    Revert "Expand Recents to the top if Search is disabled"
    This reverts commit c552e692591502a3361105b23ac04370fd58bf51.
    
    Conflicts:
    
    packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
    
    Change-Id: Ie6fa49417ba65387f8864cc8b8cef3ea2a7ac1f0

--- packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
@@ -65,10 +65,9 @@ public class RecentsConfiguration {
 
     /** Search bar */
     int searchBarAppWidgetId = -1;
-
+    public int searchBarSpaceHeightPx;
     boolean searchBarAppEnabled;
     private Context mContext;
-    public int searchBarSpaceHeightPx;
 
     /** Task stack */
     public int taskStackScrollDuration;
@@ -137,8 +136,6 @@ public class RecentsConfiguration {
         // Properties that don't have to be reloaded with each configuration change can be loaded
         // here.
 
-        mContext = context;
-
         // Interpolators
         fastOutSlowInInterpolator = AnimationUtils.loadInterpolator(context,
                 com.android.internal.R.interpolator.fast_out_slow_in);
@@ -171,7 +168,6 @@ public class RecentsConfiguration {
 
     /** Updates the state, given the specified context */
     void update(Context context) {
-        mContext = context;
         SharedPreferences settings = context.getSharedPreferences(context.getPackageName(), 0);
         Resources res = context.getResources();
         DisplayMetrics dm = res.getDisplayMetrics();
@@ -292,7 +288,6 @@ public class RecentsConfiguration {
 
     /** Updates the search bar app widget */
     public void updateSearchBarAppWidgetId(Context context, int appWidgetId) {
-        mContext = context;
         searchBarAppWidgetId = appWidgetId;
         SharedPreferences settings = context.getSharedPreferences(context.getPackageName(), 0);
         settings.edit().putInt(Constants.Values.App.Key_SearchAppWidgetId,
@@ -301,7 +296,6 @@ public class RecentsConfiguration {
 
     /** Updates the states that need to be re-read whenever we re-initialize. */
     void updateOnReinitialize(Context context, SystemServicesProxy ssp) {
-        mContext = context;
         // Check if the developer options are enabled
         developerOptionsEnabled = ssp.getGlobalSetting(context,
                 Settings.Global.DEVELOPMENT_SETTINGS_ENABLED) != 0;
@@ -376,9 +370,6 @@ public class RecentsConfiguration {
                                    Rect searchBarSpaceBounds) {
         // Return empty rects if search is not enabled and Search bar is disabled
         int searchBarSize = searchBarSpaceHeightPx;
-        if (!searchBarAppEnabled) {
-            searchBarSize = 0;
-        }
         if (!Constants.DebugFlags.App.EnableSearchLayout || !hasSearchBarAppWidget()) {
             searchBarSize = 0;
         }
